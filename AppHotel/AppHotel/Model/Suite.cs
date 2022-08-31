e0eac18af64bb352dc2be20e9693fa63474356af
Usuario <Usuario@CPD02-M07>
2022-08-31 14:07:53 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/AppHotel/Model/Suite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppHotel.Model
+{
+    public class Suite : ContentPage
+    {
+        public Suite()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
